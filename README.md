e6c3d9539136479798d52acef63ca607bc86415f
thaiquocdang <56788585+thaiquocdang@users.noreply.github.com>
2020-12-23 16:39:13 +1100

    Login page template & add Readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # StoryBooks
+
+# Install dependencies
+
+npm install
+
+# Run in development
+
+npm run dev
+
+# Run in production
+
+npm start
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const dotenv = require('dotenv')
 const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
@@ -19,6 +20,9 @@ if (process.env.NODE_ENV === 'development') {
 //Handlebars middleware
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' })) //default layout of file name 'main'
 app.set('view engine', '.hbs')
+
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 //ROUTES
 app.use('/', require('./routes/index'))
