0fdab5bb14abb48880f10e02583a5276409f11c2
thaiquocdang <56788585+thaiquocdang@users.noreply.github.com>
2020-12-27 22:27:55 +1100

    Initial setup Passport, passport google strategy

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const path = require('path')
 const dotenv = require('dotenv')
 const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
+const passport = require('./config/passport')
+const session = require('express-session')
 const morgan = require('morgan')
 
 //Load config
 dotenv.config({ path: './config/config.env' })
+
+//Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -20,6 +25,19 @@ if (process.env.NODE_ENV === 'development') {
 //Handlebars middleware
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' })) //default layout of file name 'main'
 app.set('view engine', '.hbs')
+
+//sessions
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false, //false: we dont a session if nothing is modified
+    saveUninitialized: false, //false: dont create a session untill somthing is stored
+  })
+)
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
